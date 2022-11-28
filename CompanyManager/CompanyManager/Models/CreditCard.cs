65c427b387880d58b9b8ddd1f3fffdb3ac1f7754
Lucas Granelli <granellilucas@gmail.com>
2022-11-27 21:47:03 -0300

    fixes

---
+++ CompanyManager/CompanyManager/Models/CreditCard.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyManager.Models
+{
+    public class CreditCard
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public int CardNumber { get; set; }
+
+        public string CardName { get; set; }
+
+        public int CardCVV { get; set; }
+
+    }
+}
